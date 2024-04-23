99d680417637e2de7fc93149dd6e5fdc3dd1432f
Yash Dayani <52958495+YashDayani@users.noreply.github.com>
2024-04-23 10:38:54 +0530

    23-April-24

---
+++ script.js
@@ -0,0 +1,10 @@
+const button_sin = document.getElementById('btnsin');
+
+button_sin.addEventListener('click', function () {
+    window.location.href = 'user.html';
+});
+
+const button_sup = document.getElementById('btnsup');
+button_sup.addEventListener('click', function () {
+    window.location.href = 'signin.html';
+});
